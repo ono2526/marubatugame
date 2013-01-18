55c81efed1b2e8c6f200a4ebfca13cc8e3687113
ono2526 <s.ono.2526@gmail.com>
2013-01-18 11:32:30 +0900

    課題４
    ・ソースにインデントを追加。
    ・ソースにコメントを追加

--- まるばつゲーム/まるばつゲーム.c
+++ まるばつゲーム/まるばつゲーム.c
@@ -7,7 +7,7 @@ char line1[9]="    789\n",
      line2[9]="    456\n",
      line3[9]="    123\n";
 
-int win1=0,win2=0;
+int win1=0,win2=0;  //　勝敗数の変数定義
 
 void main(){
 	
@@ -16,6 +16,7 @@ void main(){
 start:
 	turn='o';
     printf("○×ゲーム\n");
+	/*　表示する配列の初期化　*/
 	for(i=0;i<3;i++){
 	   line1[i] = ' ';
 	   line2[i] = ' ';
@@ -23,15 +24,20 @@ start:
 	}
     printf("%s%s%s\n",line1,line2,line3);
 
-	
     while(1){
       while(1){
-        if(turn=='o')printf("○先手：数字キーで指定");
-        else printf("×後手：数字キーで指定");
-        scanf("%c%*[^\n]",&c);getchar();
-        if(input(c,turn))printf("もう一度入力してください");
-        else break;
-      }
+        if(turn=='o'){
+			printf("○先手：数字キーで指定");
+		}else{
+			printf("×後手：数字キーで指定");
+		}
+		scanf("%c%*[^\n]",&c);getchar();
+        if(input(c,turn)){
+			printf("もう一度入力してください");
+		}else{
+			break;
+		}
+	  }
       printf("%s%s%s\n",line1,line2,line3);
 	  if(judge(turn)){
 		  printf("1Pの勝利数……%d回\n",win1);
@@ -42,11 +48,15 @@ start:
 			  return;
 		  }else{
 			  goto start;
-		      return;
 		  }
 	  }
-      if(turn=='o')turn='x';
-      else turn='o';
+
+	  /*　ターンの更新　*/
+      if(turn=='o'){
+		  turn='x';
+	  }else{
+		  turn='o';
+	  }
     }
 }
 
@@ -54,6 +64,7 @@ int input(char c, char piece){
   int flg=0;
   switch(c){
     case '1': if(line3[0]==' ')line3[0]=piece ;else flg=1;break;
+			  
     case '2': if(line3[1]==' ')line3[1]=piece ;else flg=1;break;
     case '3': if(line3[2]==' ')line3[2]=piece ;else flg=1;break;
     case '4': if(line2[0]==' ')line2[0]=piece ;else flg=1;break;
